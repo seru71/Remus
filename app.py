f4288e50a9873ac257ad94ae457968ea197b049e
DamianSkrzypczak <damian.piotr.skrzypczak@gmail.com>
2018-06-10 00:34:06 +0200

    changed to debug mode

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import pybedtools
+
 from remus.remus import app
 
-app.run()
+pybedtools.debug_mode(True)
+app.config.update(
+    TESTING=True,
+    DEGBUG=True,
+    ENV="development",
+    TRAP_HTTP_EXCEPTIONS=True,
+    # USE_X_SENDFILE=True,  # For Future Apache efficient file sending
+    EXPLAIN_TEMPLATE_LOADING=True,
+
+)
+
+app.run(debug=True,
+        port=5001)
+# threaded=True)
